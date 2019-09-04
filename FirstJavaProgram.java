01ba6d3e7efbf92ddf00cbe2179c718d70abc786
Umazeez <umazeezcontacts@gmail.com>
2019-09-04 21:39:20 +0100

    This is my first java program and hello to the world

---
+++ FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram {
+  public static void main(String[] args){
+    System.out.println("This is my first program in java,this is quite amazing while i improve in due course");
+  }//End of main
+}//End of FirstJavaProgram Class
---
+++ Welcome1.java
@@ -0,0 +1,6 @@
+
+public class Welcome1{
+	public static void main(String arg []){
+        System.out.printf("%s%n%s%n", "welcome to",100, "java Programming!",200);
+}
+}
