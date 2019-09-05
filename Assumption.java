5862b787f680ac451e9c77178d14e45c1bb0a878
Umazeez <umazeezcontacts@gmail.com>
2019-09-05 09:07:16 +0100

    This is wonderful

---
+++ Assumption.java
@@ -0,0 +1,11 @@
+// exercice number 4. capter 2.10
+public class Assumption {
+	public static void main(String[] args) {
+		int x=2;
+		int y=3;
+		System.out.printf( "x = %d\n", x );
+		System.out.printf("Value of %d + %d is %d%n", x, x, (x + x));
+		System.out.printf("x =");
+		System.out.printf("%d = %d%n", (x + y), (y + x));
+}
+}
